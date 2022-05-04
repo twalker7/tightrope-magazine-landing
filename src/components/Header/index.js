bf266d47f7e871c9db2d98699103fac844b9ec62
Thomas Walker <thomasnwalker@yahoo.com>
2022-05-04 02:21:19 -0700

    added mui grid components

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import AppBar from '@mui/material/AppBar'
+
+import CalendarMonthRoundedIcon from '@mui/icons-material/CalendarMonthRounded';
+
+
+function Header(){
+    return(
+        <div>
+            <AppBar>
+                
+    <Grid container spacing={30} justifyContent="center">
+        <Grid item>
+         hello world
+
+        </Grid>
+        <Grid item>
+            Wello horld
+        </Grid>                                                                                                                                                                                                                                                                    
+
+    </Grid>
+            </AppBar>
+
+        </div>
+    );
+}
+
+export default Header; 
