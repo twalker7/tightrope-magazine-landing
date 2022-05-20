7c12b5753351300405ccdbd2b6a4ab5535341b87
Thomas Walker <thomasnwalker@yahoo.com>
2022-05-20 15:34:28 -0700

    grid:

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -7,10 +7,17 @@ function Content(){
     return(
         <div>
     
-       <div className="contentContainer">
-       <h2 className="contentHeader"> Content </h2> 
-       <div className="contentBody"> </div> 
-       </div> 
+            <div className="contentContainer" >
+                    <h2 className="contentHeader"> Content </h2> 
+                    <div className="contentBody">
+                        <Grid container flexDirection="row" spacing={0}> 
+                            <Grid item lg={4}> H</Grid>
+                            <Grid item lg={4}> I </Grid>
+                            <Grid item lg={4}> G</Grid>
+                             
+                        </Grid>
+                    </div> 
+            </div> 
         </div>
     );
 }
