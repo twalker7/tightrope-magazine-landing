2cfd3924a447ecef253a30a4b2e9d7724407cd3b
Thomas Walker <thomasnwalker@yahoo.com>
2022-05-21 11:06:58 -0700

    setting up content grid

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -11,9 +11,17 @@ function Content(){
                     <h2 className="contentHeader"> Content </h2> 
                     <div className="contentBody">
                         <Grid container flexDirection="row" spacing={0}> 
-                            <Grid item lg={4}> H</Grid>
-                            <Grid item lg={4}> I </Grid>
-                            <Grid item lg={4}> G</Grid>
+                            <Grid item lg={2}>
+                                <div className="columnContain">
+                                    
+                                </div>
+                            </Grid>
+                            <Grid item lg={8}>
+                                <div className="columnContain"></div>
+                            </Grid>
+                            <Grid item lg={2}>
+                                <div className="columnContain"></div>
+                            </Grid>
                              
                         </Grid>
                     </div> 
